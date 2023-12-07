87adee3a9e7c94128ef57dc7826565db6a566437
Arnab Deep Nath <86016150+ArnabDeepNath@users.noreply.github.com>
2023-12-07 18:25:09 +0530

    Gsp App
    This is the Beta Version of the GSP Application for MeritFox Technologies

---
+++ Flutter/gspapp4/android/app/src/main/kotlin/com/example/gspapp4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gspapp4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/gspapp4/lib/code_01.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightSearchButton extends StatefulWidget {
+  const FlightSearchButton({super.key});
+
+  @override
+  State<FlightSearchButton> createState() => _FlightSearchButtonState();
+}
+
+class _FlightSearchButtonState extends State<FlightSearchButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
